433b16c76e427e023015bb69d58c70134dbf3644
Thirunavukkarasu <tamilan.arasu@gmail.com>
2021-11-21 22:21:04 +0530

    added index.js file

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const acs = require('./acs');
+const dssl = require('./dssl');
+const mlp = require('./mlp');
+const workshop = require('./workshop');
+
+module.exports = {
+    acs,
+    dssl,
+    mlp,
+    workshop
+};
