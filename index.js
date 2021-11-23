60860d9be3cdafe330862e355bdf23e2d4764ef1
Thirunavukkarasu <tamilan.arasu@gmail.com>
2021-11-23 13:52:31 +0530

    feat: :art: added web database and Lead model

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dssl = require('./dssl');
 const mlp = require('./mlp');
 const workshop = require('./workshop');
 const ctp = require('./ctp');
+const web = require('./web');
 const nucleus = require('./nucleus');
 
 module.exports = {
@@ -13,5 +14,6 @@ module.exports = {
     mlp,
     workshop,
     ctp,
+    web,
     nucleus
 };
---
+++ web/Lead.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const paginate = require('mongoose-paginate');
+const aggregatePaginate = require('mongoose-aggregate-paginate');
+
+const Schema = mongoose.Schema;
+
+// Note: Considered leadsquared predefined names so dint followed proper naming conventions
+const Lead = new Schema({
+    FirstName: String,
+    mx_State: String,
+    Phone: String,
+    mx_City: String,
+    grade: String,
+    pincode: String,
+    EmailAddress: String,
+    Website: String,
+    mx_Course_Name: String,
+    mx_Source_of_Lead: String,
+    mx_UTM_Term: String,
+    mx_UTM_Match_Type: String,
+    mx_UTM_Network: String,
+    mx_UTM_Device: String,
+    mx_UTM_Site: String,
+    mx_UTM_Ad_Position: String,
+    mx_Inbound_Enquiry_Date: String,
+    MXHOrgCode: Number,
+    MXHLandingPageId: String,
+    MXHFormBehaviour: Number,
+    MXHOutputMessagePosition: Number,
+    MXHFormDataTransfer: Number,
+    MXHRedirectUrl: String,
+    MXHAsc: Number,
+    MXHFormName: String,
+    SourceCampaign: String,
+    SourceMedium: String,
+    SourceContent: String,
+    UtmSource: String,
+    CampaignId: String,
+
+    isCustomActivity: Boolean,
+    ActivityEvent: Number,
+    ActivityNote: String,
+
+    mx_Campaign_Type: String,
+    mx_Facebook_Campaign_Date: String,
+
+    mx_Course: String,
+    mx_Sales_Circle_City: String,
+    mx_Date_of_Birth: Date,
+    mx_Educational_Qualification_UPSC: String,
+    mx_Graduation_Year: Number,
+    mx_Resume_URL: String,
+    mx_Gender: String,
+    slugName: String,
+    referralUrl: String,
+
+    mx_Corporate_Name: String,
+    mx_Corporate_Campaign_Date: String,
+    mx_Corporate_Employee_ID: String,
+    mx_Corporate_Employee_Id_Proof: String,
+    EmployeeID: String,
+    parentGaurdianIndicator: String,
+    mx_School: String,
+    childPerformance: String,
+    isGoForTuition: String,
+    takeCarerOFChild: String,
+    mx_Comment: String,
+    url: String,
+    isNumberAlreadyExistFlag: String,
+    jwtToken: String,
+    page: String,
+
+    c1h2e3c4k5d6u7p8l9i1c2a3t4e5: String,
+
+    mx_FB_Source_Campaign: String,
+    mx_utm_Term: String,
+    mx_Gclid: String,
+    mx_Fbclid: String,
+
+    countryCode: String,
+
+    isCronJobRan: Boolean,
+    lsRequestID: String,
+    lastRunCronJobDate: Date,
+
+    createdAt: Date,
+    updatedAt: Date,
+}, {
+    timestamps: true,
+    collection: 'leads'
+});
+
+Lead.plugin(paginate);
+Lead.plugin(aggregatePaginate);
+
+module.exports = Lead;
