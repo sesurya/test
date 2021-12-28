d158aab87f497fdaaf1c4d2aa7e9813c986d0b88
Muthukumar-16 <muthuabja2020@gmail.com>
2021-12-28 11:14:33 +0530

    refactor: :recycle: Renamed the files to Start Case

---
+++ ctp/StudentExamDetail.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StudentExamDetail = new Schema({
+  phoneNumber: {type: Number, required: true},
+  examName: {type: String, required: true}, // bnat, bnat-k10, bnat-premium
+  interestedIn: String, // JEE, NEET, JEE+NEET
+  active: Boolean,
+  grade: Number,
+  gradeGroup: String,
+  url: String,
+  utmCampaign: String,
+  utmSource: String,
+  utmContent: String,
+  examStartDateTime: {type: Number, required: true},
+  resultsAnnouncedDateTime: {type: Number, required: true},
+  leadSquaredActivityId: String,
+  lsqStats: Boolean,
+  examAttempted: {type: Boolean},
+}, {
+  timestamps: {
+    createdAt: 'registrationDate',
+    updatedAt: 'modifiedDate',
+  },
+  minimize: false,
+});
+
+module.exports = StudentExamDetail;
