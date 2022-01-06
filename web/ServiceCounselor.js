f0fbe2895d6a29d13284e47042120c4e403258c4
johnson-byjus <johnson.cherian@byjus.com>
2022-01-06 14:50:43 +0530

    feat: added service counselor model

---
+++ web/ServiceCounselor.js
@@ -0,0 +1,44 @@
+const ServiceCounselor = new Schema({
+    age: String,
+    email: String,
+    experience: String,
+    gender: String,
+    graduationDegree: String,
+    isResumeExist: Boolean,
+    isResumeUploaded: Boolean,
+    isVideoExist: Boolean,
+    isVideoYouTubeLinkExists: Boolean,
+    lsRequestID: String,
+    lsqStats: Boolean,
+    mobile: String,
+    mx_Current_Employer: String,
+    mx_Grad_College: Number,
+    mx_Preferred_Location: Number,
+    name: Number,
+    postAnswer: Number,
+    preferred_Location: Number,
+    presentWorkingCity: Number,
+    prospectId: Number,
+    qualification: Number,
+    referrence: Number,
+    resume: Number,
+    resumeFileNameLength: Number,
+    resumeFileSize: Number,
+    resumeFilesType: Array,
+    salaryInLakhs: String,
+    subject: Number,
+    teachingExperience: Number,
+    totalExperience: Number,
+    url: Number,
+    youTubeLink: Number,
+    createdAt: Date,
+    updatedAt: Date
+}, {
+    timestamps: true,
+    collection: 'servicecounseldatas'
+});
+
+ServiceCounselor.plugin(paginate);
+ServiceCounselor.plugin(aggregatePaginate);
+
+module.exports = ServiceCounselor;
