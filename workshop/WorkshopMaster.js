588e77c79b5a3e6cc11362ed07788f1a51bbbe0b
Suchith-G <suchith.g@byjus.com>
2022-02-23 15:30:22 +0530

    New schema merged

--- workshop/WorkshopMaster.js
+++ workshop/WorkshopMaster.js
@@ -4,6 +4,34 @@ const paginate = require('mongoose-paginate');
 const aggregatePaginate = require('mongoose-aggregate-paginate');
 const Schema = mongoose.Schema;
 
+const faqSecValue = new Schema({
+    title: String,
+    forqueries: String,
+    tollfree: Number,
+});
+
+const headerValue = new Schema({
+    headerImg: String,
+    seminarTitle: String,
+    seminarType: String,
+});
+
+const firstMidSecValue = new Schema({
+    titleTop: String,
+    titlebottom: String,
+    highlight: Array,
+});
+
+const secondMidSecValue = new Schema({
+    titleTop: String,
+    titlebottom: String,
+    highlight: Array,
+});
+
+const urlPathValue = new Schema({
+    imgUrl: String,
+});
+
 const WorkshopMaster = new Schema({
     name: String,
     formattedName: String,
@@ -14,6 +42,14 @@ const WorkshopMaster = new Schema({
     language: String,
     createdBy: String,
     updatedBy: String,
+    firstmidsectionAttributes: firstMidSecValue,
+    second: String,
+    highlight: Array,
+    secondMidSecAttributes: secondMidSecValue,
+    faqSecAttributes: faqSecValue,
+    headerAttributes: headerValue,
+    urlPathAttributes: urlPathValue,
+    button: Boolean,
 }, {
     collection: 'workshop_master',
     timestamps: true
