bbca33051cbf14d6fd06d15b66fe5e2b3b002684
MitraKumar8 <mitra.v@byjus.com>
2022-03-08 17:23:47 +0530

    feat: Added Message Templates Schema

---
+++ notification/MessageTemplate.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const paginate = require('mongoose-paginate');
+const aggregatePaginate = require('mongoose-aggregate-paginate');
+const Schema = mongoose.Schema;
+
+const MessageTemplate = new Schema({
+  gradeGroup: String,
+  templateTitle: String,
+  templateGroup: String,
+  smsTemplate: String,
+  emailTemplateSubject: String,
+  emailTemplateBody: String,
+  voiceOtpTemplate: String,
+  ivrLoop: Number,
+  ivrVoice: String,
+  speechRate: String,
+}, {
+  collection: 'msgtemplates',
+  timestamps: true
+});
+
+MsgTemplate.plugin(paginate);
+MsgTemplate.plugin(aggregatePaginate);
+
+module.exports = MessageTemplate;
