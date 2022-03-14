ba018c2bacf45ada7117b82e3c0d2536bbd8bba7
Suchith-G <suchith.g@byjus.com>
2022-03-14 16:31:08 +0530

    LODS-1499 #Comment schema updated

--- workshop/WorkshopMaster.js
+++ workshop/WorkshopMaster.js
@@ -4,54 +4,46 @@ const paginate = require('mongoose-paginate');
 const aggregatePaginate = require('mongoose-aggregate-paginate');
 const Schema = mongoose.Schema;
 
-const faqSecValue = new Schema({
-    title: String,
-    forqueries: String,
-    tollfree: Number,
-});
+// const faqSecValue = new Schema({
+//     title: String,
+//     forqueries: String,
+//     tollfree: Number,
+// });
 
-const headerValue = new Schema({
-    headerImg: String,
-    seminarTitle: String,
-    seminarType: String,
-});
+// const headerValue = new Schema({
+//     headerImg: String,
+//     seminarTitle: String,
+//     seminarType: String,
+// });
 
-const firstMidSecValue = new Schema({
-    titleTop: String,
-    titlebottom: String,
-    highlight: Array,
-});
+// const firstMidSecValue = new Schema({
+//     titleTop: String,
+//     titlebottom: String,
+//     highlight: Array,
+// });
 
-const secondMidSecValue = new Schema({
-    titleTop: String,
-    titlebottom: String,
-    highlight: Array,
-});
+// const secondMidSecValue = new Schema({
+//     titleTop: String,
+//     titlebottom: String,
+//     highlight: Array,
+// });
 
-const urlPathValue = new Schema({
-    imgUrl: String,
-});
+// const urlPathValue = new Schema({
+//     imgUrl: String,
+// });
 
 const WorkshopMaster = new Schema({
-    name: String,
-    formattedName: String,
-    header: String,
-    content: String,
-    teacherName: String,
-    teacherImg: String,
-    language: String,
-    createdBy: String,
-    updatedBy: String,
-    firstmidsectionAttributes: firstMidSecValue,
-    second: String,
-    highlight: Array,
-    secondMidSecAttributes: secondMidSecValue,
-    faqSecAttributes: faqSecValue,
-    headerAttributes: headerValue,
-    urlPathAttributes: urlPathValue,
-    button: Boolean,
+    language : String,
+    seminarTitle : String,
+    seminarType : String,
+    shcardleftdown : String,
+    shcardleftup : String,
+    shcardrightdown : String,
+    shcardrightup : String,
+    teacher : String,
+    teacherdescription : String,
 }, {
-    collection: 'iasworkshop',
+    collection: 'workshop_master',
     timestamps: true
 });
 
