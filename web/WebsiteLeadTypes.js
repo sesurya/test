747cdca54961db753a5e38ebacfb72fddcc6be6b
Bharathyuvi <bharath.p4@byjus.com>
2022-03-15 22:46:56 +0530

    LDOS-1954 #comment created new collection websiteLeadTypes

---
+++ web/WebsiteLeadTypes.js
@@ -0,0 +1,17 @@
+'use strict';
+const mongoose = require('mongoose');
+const paginate = require('mongoose-paginate');
+const aggregatePaginate = require('mongoose-aggregate-paginate');
+const Schema = mongoose.Schema;
+const WebsiteLeadTypes = new Schema({
+    name:String,
+    formattedName: String,
+    description:String,
+    createdAt:Date,
+    updatedAt:Date,
+    createdBy:String,
+    updatedBy:String
+});
+WebsiteLeadTypes.plugin(paginate);
+WebsiteLeadTypes.plugin(aggregatePaginate);
+module.exports = WebsiteLeadTypes;
