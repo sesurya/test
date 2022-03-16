53462f6f421270b56020a06a1416a6f01f381923
Suchith-G <suchith.g@byjus.com>
2022-03-16 13:36:38 +0530

    LODS-1449 #comment Chnages made in WorkshopMaster

--- workshop/WorkshopMaster.js
+++ workshop/WorkshopMaster.js
@@ -6,25 +6,23 @@ const Schema = mongoose.Schema;
 
 
 const WorkshopMaster = new Schema({
-    language: String,
-    seminarHighlights: {
-        leftcard: {
-            leftone: { icon: String, content: String },
-            lefttwo: { icon: String, content: String },
-            leftthree: { icon: String, content: String }
+    name: String,
+    formattedName: String,
+    pageConfig: {
+        language: String,
+        seminarHighlights: {
+            leftcard: [{ type: Array, "default": [] }],
+            rightcard: [{ type: Array, "default": [] }]
         },
-        rightcard: {
-            rightone: { icon: String, content: String },
-            righttwo: { icon: String, content: String },
-            rightthree: { icon: String, content: String }
-        },
-    },
-    seminarTitle: String,
-    seminarType: String,
-    faqSection: { faq1: { faq: String, solution: String }, faq2: { faq: String, solution: String }, faq3: { faq: String, solution: String } },
-    teacher: String,
-    teacherdescription: String,
-    teacherimage: String
+        seminarTitle: String,
+        seminarDescription: String,
+        faqSection: { type: Array, "default": [] },
+        teacher: String,
+        teacherdescription: String,
+        teacherimage: String
+    }
+
+
 }, {
     collection: 'workshop_master',
     timestamps: true
@@ -34,3 +32,5 @@ WorkshopMaster.plugin(paginate);
 WorkshopMaster.plugin(aggregatePaginate);
 
 module.exports = WorkshopMaster;
+
+
