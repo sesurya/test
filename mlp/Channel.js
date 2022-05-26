cc0ba322bff21b8269b2d7b487d01bc0330c665d
Ravi Khare <ravi.khare@byjus.com>
2022-05-26 11:59:41 +0530

    Updated Channel Collection

--- mlp/Channel.js
+++ mlp/Channel.js
@@ -1,20 +1,23 @@
 'use strict';
+
 const mongoose = require('mongoose');
 const paginate = require('mongoose-paginate');
 const aggregatePaginate = require('mongoose-aggregate-paginate');
 const Schema = mongoose.Schema;
+
 const Channel = new Schema({
-    name:String,
-    formattedName: String,
-    description:String,
-    createdAt:Date,
-    updatedAt:Date,
-    createdBy:String,
-    updatedBy:String,
-    codePrefix:String,
-}, {
-    timestamps: true,
-    collection: 'channels'
+    name: { type: String, required: true },
+    formattedName: { type: String, required: true, unique: true },
+    codePrefix: { type: Number, required: true, unique: true },
+    description: { type: String, required: true },
+    createdBy: { type: String },
+    updatedBy:{ type: String },
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
+}, 
+{
+    collection: 'channels',
+    timestamps: true
 });
 Channel.plugin(paginate);
 Channel.plugin(aggregatePaginate);
