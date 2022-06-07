e24937e5879034e1d5a05592528a303ab08be1f1
Abdul Salim <abdul.salim@byjus.com>
2022-06-07 16:00:35 +0530

    WTT-3456 #comment Create new model for quiz data

---
+++ web/ByjusQuiz.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const paginate = require('mongoose-paginate');
+const aggregatePaginate = require('mongoose-aggregate-paginate');
+const Schema = mongoose.Schema;
+
+const ByjusQuiz = new Schema({
+    slug_name: String,
+    question_part_1: String,
+    answer_opt_1: String,
+    answer_opt_2: String,
+    answer_opt_3: String,
+    answer_opt_4: String,
+    correct_answer: String,
+    answer_explanation: String,
+    is_enabled: Boolean,
+    is_deleted: Boolean,
+
+    created_at: String,
+    updated_at: String,
+
+}, {
+    timestamps: true,
+    collection: 'byjusquiz'
+});
+
+ByjusQuiz.plugin(paginate);
+ByjusQuiz.plugin(aggregatePaginate);
+
+module.exports = ByjusQuiz;
