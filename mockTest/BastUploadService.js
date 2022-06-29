85c83d77fbb2c8b03a90d37539ded7623fd499c9
Mohan Kumar H P <mohan.p2@byjus.com>
2022-06-29 14:35:57 +0530

    feat: :art: New Schema for Bast

---
+++ mockTest/BastUploadService.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const paginate = require('mongoose-paginate');
+const aggregatePaginate = require('mongoose-aggregate-paginate');
+const Schema = mongoose.Schema;
+
+const BastUploadService = new Schema({
+  uploadedBy: String,
+  filename: String,
+  url: String,
+}, {
+  collection: 'bastuploadservices',
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+});
+
+BastUploadService.plugin(paginate);
+BastUploadService.plugin(aggregatePaginate);
+
+module.exports = BastUploadService;
