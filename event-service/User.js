246bbff4bf1c00d603c93269eb92bbdb1211975c
Akshay Kamate <88528411+akshay-byjus@users.noreply.github.com>
2022-07-13 21:22:50 +0530

    feat: :sparkles: New event service User modal added

---
+++ event-service/User.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const paginate = require('mongoose-paginate');
+const aggregatePaginate = require('mongoose-aggregate-paginate');
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  userId: {       // uuid
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: { type: String, required: true },
+  name: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  state: { type: String, required: true },
+  city: { type: String, required: true },
+  grade: { type: String, required: true },
+  password: { type: String, required: true },
+  hashedPassword: { type: String, required: true },
+  eoGroupId: { type: String, required: true }, // uuid
+  isActive: Boolean,
+}, {
+  collection: 'users',
+  timestamps: true,
+});
+
+User.plugin(paginate);
+User.plugin(aggregatePaginate);
+
+module.exports = User;
